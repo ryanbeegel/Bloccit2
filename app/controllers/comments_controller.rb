c4898403ade3f2729eb2b32e881e4fa2058a2e59
vagrant <vagrant@rails-dev-box>
2014-06-24 19:35:45 +0000

    Added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,27 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.build(params.require(:comment).permit(:body, :post_id))
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    @comment = Comment.new
+
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = "Your comment was created"
+    else
+      flash[:error] = "Whoops! There was a problem saving this comment."
+    end
+ 
+    respond_with(@comment) do |f|
+      f.html {redirect_to [@topic, @post]}
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
   end
 end
